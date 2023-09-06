edfe9c79ae0567f529dd7644fafc4cacc9e890fc
wendy dias <wendyalvarengad@hotmail.com>
2023-09-06 00:28:13 -0300

    menu + seção infos

--- ghibli-movies/src/App.jsx
+++ ghibli-movies/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
 import Card from './components/Card'
+import Infos from './components/Infos'
 
 function App() {
 
   return (
     <>
+    <header> 
+      <img className='totoro' src='/totoro.png' /> 
+      <ul className='navbar'>
+        <li> Movies </li>
+        <li> Infos </li>
+        <li> Curiosities </li>
+      </ul>
+    </header>
       <h1> Studio Ghibli - スタジオジブリ作品 </h1>
       <section className ='cards'>
         <Card />
+      </section>
+
+      <h2> Infos </h2>
+
+      <section className='infos'>
+        <Infos />
       </section>
     </>
   )
---
+++ ghibli-movies/src/components/Infos/index.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import styles from './Infos.module.css'
+
+function Infos(){
+
+    const [infos, setInfos] = useState([])
+
+    useEffect(()=> {
+        const buscarInfos = async () => {
+            const response = await fetch('https://ghibli.rest/films?')
+            const data = await response.json()
+            setInfos(data)
+        }
+        buscarInfos()
+    }, [])
+
+    return(
+        infos.map( info => (
+            <section className={styles.infos}>
+                <div className={styles.title_banner}>
+                    <h2> {info.title} </h2>
+                    <img className={styles.banner} src={info.movie_banner} />
+                </div>
+                <div className={styles.all_details}>
+                    <h3 className={styles.description}> {info.description} </h3>
+                    <p className={styles.details}> Director: {info.director} </p>
+                    <p className={styles.details}> Producer: {info.producer} </p>
+                    <p className={styles.details}> {info.release_date} </p>
+                    <p className={styles.details}> {info.running_time} minutes </p>
+                    <p className={styles.details}> <img className={styles.rotten} src='./assets/Rotten_Tomatoes.png' /> {info.rt_score} </p>
+                </div>
+
+            </section>
+                    ))
+        
+    )
+
+}
+
+export default Infos
