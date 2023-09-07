178444bd5df5d6e04f0c4601d945a5502880596a
wendy dias <wendyalvarengad@hotmail.com>
2023-09-06 23:02:50 -0300

    route fixes

--- ghibli-movies/src/pages/Curiosities/index.jsx
+++ ghibli-movies/src/pages/Curiosities/index.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
 import styles from './Curiosities.module.css'
+import { Link } from 'react-router-dom'
 
 function Curiosities() {
     const [chars, setChars] = useState([])
     const [currentIndex, setCurrentIndex] = useState(null)
-    const [filmTitle, setFilmTitle] = useState('')
 
     useEffect(() => {
         const buscarChars = async () => {
@@ -20,25 +20,17 @@ function Curiosities() {
         buscarChars()
     }, [])
 
-    const nextChar = async () => {
+    
+
+    const nextChar = () => {
         let newIndex = currentIndex
         while (newIndex === currentIndex) {
             newIndex = Math.floor(Math.random() * chars.length)
         }
         setCurrentIndex(newIndex)
-
-        // Verifique se o personagem tem filmes associados
-     
-            const filmURL = chars.films[0] // Pega a primeira URL de filme
-            try {
-                const response = await fetch(filmURL)
-                const filmData = await response.json()
-                setFilmTitle(filmData.title)
-            } catch (error) {
-                console.error('Erro ao buscar detalhes do filme:', error)
-                setFilmTitle('Título não encontrado')
-            }
     }
+    
+        const filmId = chars[currentIndex]?.films
 
     return (
         <section key={chars[currentIndex]?.id}>
@@ -47,10 +39,11 @@ function Curiosities() {
             <h3 className={styles.chars}>Age: {chars[currentIndex]?.age}</h3>
             <h3 className={styles.chars}>Eye Color: {chars[currentIndex]?.eye_color}</h3>
             <h3 className={styles.chars}>Hair Color: {chars[currentIndex]?.hair_color}</h3>
-            <h3 className={styles.chars}>Films: {filmTitle}</h3>
+            <h3 className={styles.chars}><a href= {chars[currentIndex]?.films}> Movie </a></h3>
             <button className={styles.button} onClick={nextChar}>Next</button>
         </section>
     )
+    
 }
 
 export default Curiosities
--- ghibli-movies/src/pages/Infos/index.jsx
+++ ghibli-movies/src/pages/Infos/index.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import styles from './Infos.module.css'
-import { useParams } from 'react-router-dom'
 
 function Infos(){
 
