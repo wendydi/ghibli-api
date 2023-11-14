1fc6b61ff4560c145f76fff0ed0637069e90178e
wendy dias <wendyalvarengad@hotmail.com>
2023-11-14 09:25:46 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AppRoutes from './routes'
+
+function App() {
+  const url = "https://ghibliapi.vercel.app/films"
+  const
+
+  return (
+    <>
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
